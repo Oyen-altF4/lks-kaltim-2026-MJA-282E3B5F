dc8afb793487e67abf802ef0dea93528ece5fe23
Jubair-altF4 <mjubair4957@gmail.com>
2026-06-25 07:56:52 +0800

    Delete terraform/environments/production directory

--- terraform/environments/production/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "db_username"      { type = string; sensitive = true }
-variable "db_password"      { type = string; sensitive = true }
-variable "container_image"  { type = string }
-variable "jwt_secret"       { type = string; sensitive = true }
