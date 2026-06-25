5f037fdc2d19025e1a57fe19165a82666b77833f
Jubair-altF4 <mjubair4957@gmail.com>
2026-06-25 08:03:33 +0800

    Add files via upload

---
+++ terraform/modules/ecs/variables.tf
@@ -0,0 +1,13 @@
+variable "project_name"       { type = string }
+variable "environment"        { type = string }
+variable "aws_region"         { type = string }
+variable "vpc_id"             { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "alb_target_group"   { type = string }
+variable "alb_security_group" { type = string }
+variable "container_port"     { type = number; default = 3000 }
+variable "container_image"    { type = string }
+variable "task_cpu"           { type = number; default = 256 }
+variable "task_memory"        { type = number; default = 512 }
+variable "desired_count"      { type = number; default = 1 }
+variable "env_vars"           { type = map(string); default = {} }
